21f5b328720cfa4d835d19f07ff8c89d9f9670cf
Jing Xun Lai <kevin50406418@users.noreply.github.com>
2015-12-24 22:02:31 +0800

    BUG FIX

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var quest = new Array("Q1", "Q2","Q3","Q4","Q5","Q6","Q7","Q8","Q9","QA","QB","QC","QD","QE","QF","QG","QH","QI","QJ","QK");
+var quest = new Array("Q1", "Q2","Q3","Q4","Q5","Q6","Q7","Q8","Q9","QA","QB","QC","QD","QE","QF","QG","QH","QI","QJ","QK","QL","QM");
 quest.forEach(function(entry) {
-	var rnd = Math.floor((Math.random() * 2) + 1);
+	var rnd = Math.floor((Math.random() * 2));
 	document.getElementsByName(entry)[rnd].checked = true;
 });
