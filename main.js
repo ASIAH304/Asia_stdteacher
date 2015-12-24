2efa3a4f8012d3a9c9abec8427ee5b163007b8f9
Jing Xun Lai <kevin50406418@users.noreply.github.com>
2015-12-24 22:03:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var quest = new Array("Q1", "Q2","Q3","Q4","Q5","Q6","Q7","Q8","Q9","QA","QB","QC","QD","QE","QF","QG","QH","QI","QJ","QK","QL","QM");
+var quest = new Array("Q1", "Q2","Q3","Q4","Q5","Q6","Q7","Q8","Q9","QA","QB","QC","QD","QE","QF","QG","QH","QI","QJ","QK","QL","QM","QO");
 quest.forEach(function(entry) {
 	var rnd = Math.floor((Math.random() * 2));
 	document.getElementsByName(entry)[rnd].checked = true;
